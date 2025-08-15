4916354278b93177d5395a61624b99e5f421613e
Andres <marianoandres191200@hotmail.com>
2025-08-14 23:58:04 -0300

    save

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const express = require("express");
 const admin = require("firebase-admin");
 const fetch = require("node-fetch");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
@@ -21,7 +22,7 @@ app.listen(PORT, () => {
 
 admin.initializeApp({
   credential: admin.credential.cert(require("./firebase-key.json")),
-  databaseURL: "https://mcp-chat-52e15-default-rtdb.firebaseio.com",
+  databaseURL: process.env.DATABASE_URL,
 });
 
 const db = admin.database();
@@ -31,6 +32,8 @@ app.post("/prompt", async (req, res) => {
   try {
     const snapshot = await db.ref("messages").once("value");
     const messages = snapshot.val();
+
+    console.log('Pregunta: ', req.body.prompt);
 
     if (!messages) return res.status(404).json({ error: "No messages found" });
 
@@ -44,11 +47,17 @@ app.post("/prompt", async (req, res) => {
       })
       .join("\n");
 
-    const targetPrompt = `Hola, estos son los mensajes de los usuarios en un chat: 
-        ${messagesText}
+    const targetPrompt = `Hola, eres un asistente que lee mensajes de un chat y decide si debe ejecutar una acción. Las acciones posibles son:
+    1 - saveOffUser(usuario, fecha, descripción). Esta acción se va a ejecutar cuando haya algun mensaje que contenga "no voy a estar" o "voy a estar off". La descripción va a ser el motivo por el que no esté el usuario, si es que lo aclara
+    2 - ninguna (si no hay accion que ejecutar) 
 
-        Y ahora te pregunto sobre esos mensajes: ${prompt}.
-        Respondé de forma clara y concisa por favor.
+    Responde en formato JSON según el evento y con este formato. Por ejemplo si el tool es saveOffUser:
+    {
+      "tool": "saveOffUser", "params": {"user": "Usuario", "date": "la fecha en la que no va a estar el usuario", "reason": "No estará por vacaciones el dia 18 de agosto"}
+    }
+        Los mensajes del chat son estos: ${messagesText}
+
+        Y la pregunta es: ${prompt}.
         `;
 
     const response = await fetch("http://localhost:11434/api/generate", {
@@ -62,7 +71,9 @@ app.post("/prompt", async (req, res) => {
     });
 
     const data = await response.json();
-    
+    console.log("RESPUESTA DE OLLAMA:");
+    console.log(data);
+
     return res.json({ answer: data.response.trim() });
   } catch (error) {
     console.error(error);
